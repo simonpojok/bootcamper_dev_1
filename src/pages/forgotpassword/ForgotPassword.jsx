e1bf258c17857926a5f3eb88b578ecac267faa71
Pauline Nalwanga <paulinon141@gmail.com>
2024-09-09 17:52:08 +0300

    add bg image and starting word

--- src/pages/forgotpassword/ForgotPassword.jsx
+++ src/pages/forgotpassword/ForgotPassword.jsx
@@ -1,7 +1,18 @@
+import backgroundImageUrl from '../../assets/images/bootcamp/bootcamp-background.jpg'
+// import Formik from 'formik';
+
 export default function ForgotPassword () {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="min-h-screen bg-cover bg-center"
+        style={{backgroundImage: `url(${backgroundImageUrl})`}}>
+            <div className="flex flex-col justify-center items-center min-h-screen bg-black bg-opacity-50">
+                <h1 className="text-4xl text-white font-bold mb-8">Forgot Password</h1>
+                <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+                    <h2 className="text-2xl font-semibold text-blue-600 text-center mb-6">Enter EMail Address</h2>
+
+                    
+                </div>
+            </div>
         </div>
     );
 
