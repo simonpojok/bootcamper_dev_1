aa4bec357be497f1ecace4e79459f514e14d13ea
OscarPiddo <piddooscar@gmail.com>
2024-09-23 14:17:33 +0300

    Update Profile

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -1,54 +1,46 @@
 import { Formik } from "formik";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import customInstance from "../../axios_http_client";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2ZTE2MjJlNmE2ZDFkYWI3MzNlNTRmMSIsImlhdCI6MTcyNjA0Njc2NywiZXhwIjoxNzI4NjM4NzY3fQ.hqAq-LuKLSzh04u0a40r3di2RWz92uQ6DOq37fQMAqE";
+const defaultProfilePic = "https://via.placeholder.com/150"; // Placeholder image URL
+
 const url = "/api/v1/auth/updatedetails";
 
 const UpdateProfile = () => {
-  const apiEndpoint = "/api/v1/auth/updatedetails";
-  const [user, setUser] = useState([]);
-
-  useEffect(function () {
-    async function updateDetails() {
-      const data = await customInstance.put(url, {
-        name: values.username,
-        password: values.password,
-        email: values.email,
-        role: values.role,
-      });
-      console.log(data);
-    }
-  }, []);
+  const [profilePicPreview, setProfilePicPreview] = useState(defaultProfilePic);
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-900">
       <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-semibold text-white mb-6">Update Profile</h2>
+        <h2 className="text-2xl font-semibold text-white mb-6 text-center">
+          Update Profile
+        </h2>
         <Formik
           initialValues={{
             name: "",
             email: "",
-            password: "",
             role: "",
+            profilePicture: null,
           }}
           onSubmit={async (values, { setSubmitting }) => {
             try {
-              const response = await customInstance.put(
-                url,
-                {
-                  name: values.name,
-                  email: values.email,
-                  password: values.password,
-                  role: values.role,
+              const userToken = localStorage.getItem("user_token");
+              const formData = new FormData();
+              formData.append("name", values.name);
+              formData.append("email", values.email);
+              formData.append("role", values.role);
+              if (values.profilePicture) {
+                formData.append("profilePicture", values.profilePicture);
+              }
+
+              const response = await customInstance.put(url, formData, {
+                headers: {
+                  Authorization: `Bearer ${userToken}`,
+                  "Content-Type": "multipart/form-data",
                 },
-                {
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                  },
-                }
-              );
+              });
 
               console.log("Response data:", response.data.data);
             } catch (error) {
@@ -58,8 +50,44 @@ const UpdateProfile = () => {
             }
           }}
         >
-          {({ values, errors, handleSubmit, handleChange, isSubmitting }) => (
+          {({ values, handleSubmit, setFieldValue, isSubmitting }) => (
             <form onSubmit={handleSubmit} className="space-y-6">
+              <div className="flex flex-col items-center relative">
+                {/* Profile Picture */}
+                <div className="relative">
+                  <img
+                    src={profilePicPreview}
+                    alt="Profile Preview"
+                    className="w-24 h-24 rounded-full object-cover"
+                  />
+
+                  {/* Camera Icon */}
+                  <div className="absolute bottom-0 right-0 bg-gray-700 p-2 rounded-full cursor-pointer">
+                    <FontAwesomeIcon icon={faCamera} className="text-white" />
+                  </div>
+                </div>
+
+                {/* File Input */}
+                <input
+                  type="file"
+                  name="profilePicture"
+                  accept="image/*"
+                  onChange={(event) => {
+                    const file = event.target.files[0];
+                    if (file) {
+                      setFieldValue("profilePicture", file);
+                      const reader = new FileReader();
+                      reader.onloadend = () => {
+                        setProfilePicPreview(reader.result);
+                      };
+                      reader.readAsDataURL(file);
+                    }
+                  }}
+                  className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                />
+                <span className="text-sm text-gray-300 mt-2">Change Profile Picture</span>
+              </div>
+
               <div>
                 <label
                   htmlFor="name"
@@ -71,11 +99,10 @@ const UpdateProfile = () => {
                   type="text"
                   name="name"
                   value={values.name}
-                  onChange={handleChange}
+                  onChange={(e) => setFieldValue("name", e.target.value)}
                   className="mt-1 block w-full p-2.5 rounded-lg bg-gray-700 text-white border border-gray-600 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Enter your username"
                 />
-                {errors.name && <small className="text-red-500">{errors.name}</small>}
               </div>
 
               <div>
@@ -89,31 +116,10 @@ const UpdateProfile = () => {
                   type="email"
                   name="email"
                   value={values.email}
-                  onChange={handleChange}
+                  onChange={(e) => setFieldValue("email", e.target.value)}
                   className="mt-1 block w-full p-2.5 rounded-lg bg-gray-700 text-white border border-gray-600 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Enter your email"
                 />
-                {errors.email && <small className="text-red-500">{errors.email}</small>}
-              </div>
-
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-300"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  value={values.password}
-                  onChange={handleChange}
-                  className="mt-1 block w-full p-2.5 rounded-lg bg-gray-700 text-white border border-gray-600 focus:border-blue-500 focus:ring-blue-500"
-                  placeholder="Enter your password"
-                />
-                {errors.password && (
-                  <small className="text-red-500">{errors.password}</small>
-                )}
               </div>
 
               <div>
@@ -127,17 +133,16 @@ const UpdateProfile = () => {
                   type="text"
                   name="role"
                   value={values.role}
-                  onChange={handleChange}
+                  onChange={(e) => setFieldValue("role", e.target.value)}
                   className="mt-1 block w-full p-2.5 rounded-lg bg-gray-700 text-white border border-gray-600 focus:border-blue-500 focus:ring-blue-500"
                   placeholder="Enter your role"
                 />
-                {errors.role && <small className="text-red-500">{errors.role}</small>}
               </div>
 
               <button
                 type="submit"
                 disabled={isSubmitting}
-                className="w-full py-2.5 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full py-2.5 px-4 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 "
               >
                 {isSubmitting ? "Submitting..." : "Submit"}
               </button>
